12b7d77affbd27c5513bf9237df54946f3bd020e
enocol <eno_col@yahoo.com>
2024-02-06 22:22:20 +0000

    Edit scripts.js, display.js and styles.css

--- assets/js/displayData.js
+++ assets/js/displayData.js
@@ -1,6 +1,12 @@
 function displaydata(data) {
   const cityName = document.querySelector(".details h2");
+  const temperature = document.getElementById("temperature");
   cityName.textContent = data.name;
+  temperature.textContent = Math.round(data.main.temp - 273);
+  const windSpeed = document.getElementById("wind-speed");
+  const humidity = document.getElementById("humidity");
+  windSpeed.textContent = data.wind.speed;
+  humidity.textContent = data.main.humidity;
 }
 
 export default displaydata;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,38 @@
 import displaydata from "../js/displayData.js";
+import { resetInput } from "../js/resetInput.js";
 
 const cityInput = document.querySelector(".city-input");
 
 const searchButton = document.querySelector(".search-btn");
-const currentWeatherDiv = document.querySelector(".current-weather");
-const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "afef0a4b6a2c5afed7f5da4a756f41d9";
+// const API_KEY = "afef0a4b6a2c5afed7f5da4a756f41d9";
 const apiKey = "a87753a0f261d957d6d0e8f464a72ec2";
 
 function getWeatherData() {
   const cityName = cityInput.value.trim().toLowerCase();
+  if (cityName === "") {
+    const error = document.getElementById("error");
+    error.textContent = "Please enter a city name";
+    setTimeout(() => {
+      error.textContent = "";
+    }, 2000);
+    return;
+  }
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
+      if (data.cod >= "404" && data.cod < "500") {
+        const error = document.getElementById("error");
+        error.textContent = "City not found";
+        setTimeout(() => {
+          error.textContent = "";
+        }, 2000);
+        return;
+      }
+      displaydata(data);
+      resetInput();
       console.log(data);
-      console.log(displaydata(data));
     });
 }
 
