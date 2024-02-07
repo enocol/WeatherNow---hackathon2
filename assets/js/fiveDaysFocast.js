bdf33e0db4cc77fa986d3404dda197db6a56e1c7
enocol <eno_col@yahoo.com>
2024-02-07 11:35:46 +0000

    Edit fiveDaysFocast.js

--- assets/js/fiveDaysFocast.js
+++ assets/js/fiveDaysFocast.js
@@ -7,24 +7,20 @@ const humidity = document.querySelectorAll(".card .humidity");
 const currentCity = document.getElementById("current-city");
 
 export async function fiveDaysFocast() {
+  let waitingData = document.getElementById("error");
+  waitingData.innerText = "Loading...";
   const cityName = await getCurrentCity();
   const apiKey = "a87753a0f261d957d6d0e8f464a72ec2";
   const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
   const response = await fetch(queryURL);
   const data = await response.json();
   currentCity.textContent = cityName;
-  console.log(data);
+
   for (let i = 0; i < 5; i++) {
-    if (!data) {
-      h3[i].innerText = "loding...";
-      temperature[i].innerText = "loding...";
-      wind[i].innerText = "loding...";
-      humidity[i].innerText = "loding...";
-    } else {
-      h3[i].innerText = data.list[i].dt_txt;
-      temperature[i].innerText = Math.round(data.list[i].main.temp - 273);
-      windSpeed[i].innerText = data.list[i].wind.speed;
-      humidity[i].innerText = data.list[i].main.humidity;
-    }
+    h3[i].innerText = data.list[i].dt_txt;
+    temperature[i].innerText = Math.round(data.list[i].main.temp - 273);
+    windSpeed[i].innerText = data.list[i].wind.speed;
+    humidity[i].innerText = data.list[i].main.humidity;
+    waitingData.innerText = "";
   }
 }
