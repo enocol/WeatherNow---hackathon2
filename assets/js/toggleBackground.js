dcadf86273ee4392e75a78f0190cf544dfacf49a
enocol <eno_col@yahoo.com>
2024-02-09 09:54:43 +0000

    Edit style, index.html and toggleBackground function

--- assets/js/toggleBackground.js
+++ assets/js/toggleBackground.js
@@ -1,12 +1,15 @@
 export function toggleBackground() {
   let toggleButton = document.getElementById("toggle-background");
+  let ball = document.getElementById("tb-ball");
 
   toggleButton.classList.toggle("justify-end");
   if (toggleButton.classList.contains("justify-end")) {
-    document.body.style.backgroundColor = "grey";
-    toggleButton.style.backgroundColor = "grey";
+    document.body.style.backgroundColor = "gray";
+    toggleButton.style.backgroundColor = "black";
+    ball.style.backgroundColor = "white";
   } else {
     document.body.style.backgroundColor = "white";
     toggleButton.style.backgroundColor = "white";
+    ball.style.backgroundColor = "black";
   }
 }
